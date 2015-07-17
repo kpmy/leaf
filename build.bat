91008405ddd2e451e3f4db1694b09e3895998def
kpmy <petryxa.clever@gmail.com>
2015-07-17 15:57:47 +0300

    добавил еще один пример, даже не пришлось баги отлавливать

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 go build leaf.go
 go test
-leaf -b="Log Core Objects HostObjects HostConsole Init TestEmpty TestFib TestFact TestBubble TestObjects TestEvents TestHandler"
+leaf -b="Log Core Objects HostObjects HostConsole Init TestEmpty TestFib TestFact TestErast TestBubble TestObjects TestEvents TestHandler"
 pause
